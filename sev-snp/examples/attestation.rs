0c2011aeddd0212a334172dcb006051d2b0ba936
ameanasad <amean.asad1999@gmail.com>
2025-06-20 20:16:37 -0700

    fixes

--- sev-snp/examples/attestation.rs
+++ sev-snp/examples/attestation.rs
@@ -6,11 +6,11 @@ fn main() {
 
     // Retrieve an attestation report with default options passed to the hardware device
     let (report, _) = sev_snp.get_attestation_report().unwrap();
-    let raw_bytes = bincode::serialize(&report.clone())?;
-    println!("Attestation_bytes {}", raw_bytes);
+    let raw_bytes = bincode::serialize(&report.clone()).unwrap();
+    println!("Attestation_bytes {:?}", raw_bytes);
     // println!("Attestation Report: {}", report);
 
-    let rep: AttestationReport = bincode::deserialize(&raw_bytes.clone())?;
+    let rep: AttestationReport = bincode::deserialize(&raw_bytes.clone()).unwrap();
 
     // Verify the attestation report
     sev_snp.verify_attestation_report(&rep, None).unwrap();
